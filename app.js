5acc3a5cf5c6f83e3ec4ed11b35b342b5b891180
MEGAN ARNELL <arnell.megan@yahoo.com>
2017-02-28 14:19:31 -0700

    images change now, not sure how to get rid of the delay between pics

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ app.controller('MainCtrl', ['$scope', 'constellations', function($scope, constel
   
   $scope.selectedConstellationIndex = -1;
   
+  $scope.backgroundImg = "background/Background.jpg";
+  
+  $scope.constellationFacts = null;
+  
   $scope.setSelected = function(index) {
     $scope.selectedConstellationIndex = index;
     $scope.constellationFacts = $scope.constellations[index];
+    $scope.backgroundImg = $scope.constellations[index].img;
+    $('.container').css('background', 'url(' + $scope.backgroundImg +')');
   };
-  
-  $scope.constellationFacts = null;
   
 }]);
 
@@ -20,35 +24,43 @@ app.service('constellations', function(){
     return [
     {
       name:'Aries',
-      url:'partials/Aries.html'
+      url:'partials/Aries.html',
+      img:'background/Aries.jpg'
     }, 
     {
       name:'Cassiopeia',
-      url: 'partials/Cassiopeia.html'
+      url: 'partials/Cassiopeia.html',
+      img: 'background/Cassiopeia.jpg'
     },
     {
       name: 'Cepheus',
-      url: 'partials/Cepheus.html'
+      url: 'partials/Cepheus.html',
+      img: 'background/Cepheus.jpg'
     },
     {
       name: 'Draco',
-      url: 'partials/Draco.html'
+      url: 'partials/Draco.html',
+      img: 'background/Draco.jpg'
     }, 
     {
       name: 'Gemini',
-      url: 'partials/Gemini.html'
+      url: 'partials/Gemini.html',
+      img: 'background/Gemini.jpg'
     },
     { 
       name: 'Orion',
-      url: 'partials/Orion.html'
+      url: 'partials/Orion.html',
+      img: 'background/Orion.jpg'
     }, 
     {
       name: 'Ursa-Major',
-      url: 'partials/Ursa-Major.html'
+      url: 'partials/Ursa-Major.html',
+      img: 'background/Ursa-Major.jpg'
     }, 
     {
       name: 'Ursa-Minor',
-      url: 'partials/Ursa-Minor.html'
+      url: 'partials/Ursa-Minor.html',
+      img: 'background/Ursa-Minor.jpg'
     }
   ];
   };
